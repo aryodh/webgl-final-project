5dbae366ab7117a22938ed112c22a61129ffb2da
Fadhlan Hafizh <fadhlan.hafizh@ui.ac.id>
2020-06-12 19:14:21 +0700

    rot left done

--- LightMapDemoScene.js
+++ LightMapDemoScene.js
@@ -1759,9 +1759,14 @@ LightMapDemoScene.prototype._Update = function (dt) {
   if (this.PressedKeys.RotLeft && !this.PressedKeys.RotRight) {
     if (freeLook) {
       this.camera.rotateRight((dt / 1000) * this.RotateSpeed);
+      camWallTheta -= (dt / 1000) * this.RotateSpeed * (180 / Math.PI);
     } else {
       this.camera.rotateRight((dt / 1000) * this.RotateSpeed);
-      me.initNodes("WalleTorsoMesh", (dt / 1000) * this.RotateSpeed);
+      console.log((dt / 1000) * this.RotateSpeed);
+      me.initNodes(
+        "WalleTorsoMesh",
+        (dt / 1000) * this.RotateSpeed * (180 / Math.PI)
+      );
     }
   }
 
